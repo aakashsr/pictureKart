8a3c02c5a1e131a283b628ce6be3528145510aae
unknown <aakashsri.dev@gmail.com>
2021-07-21 19:01:29 +0530

    added the initial folder structure and setup the router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+import { Route, Switch, Link } from "react-router-dom";
+import Photos from "./pages/Photos";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" render={() => <Photos />} />
+        <Route path="/cart" render={() => <Cart />} />
+      </Switch>
     </div>
   );
 }
